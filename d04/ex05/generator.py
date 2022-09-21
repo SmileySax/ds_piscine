b422ccfec9d8e0857bf592c4a56250133b4e58f3
Kari Eaton <keaton@il-f1.msk.21-school.ru>
2022-09-21 17:06:13 +0300

    full day2

---
+++ d04/ex05/generator.py
@@ -0,0 +1,27 @@
+import sys
+import resource
+
+
+def func(path: str):
+    with open(path, 'r') as file:
+        line = 'smth'
+        while line:
+            line = file.readline()
+            yield line
+
+
+def main(path: str):
+    lines = func(path)
+    for line in lines:
+        pass
+
+    resource_usage = resource.getrusage(resource.RUSAGE_SELF)
+    mem = resource_usage.ru_maxrss / (1 << 30)
+    times = resource_usage.ru_utime + resource_usage.ru_stime
+    print(f'Peak Memory Usage = {mem:.3f} GB')
+    print(f'User Mode Time + System Mode Time = {times:.2f}s')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        main(sys.argv[1])
---
+++ d04/ex05/ordinary.py
@@ -0,0 +1,25 @@
+import sys
+import resource
+
+
+def func(path: str) -> list:
+    with open(path, 'r') as data:
+        return data.readlines()
+
+
+def main(path: str):
+    lines = func(path)
+    for line in lines:
+        pass
+
+
+    resource_usage = resource.getrusage(resource.RUSAGE_SELF)
+    mem = resource_usage.ru_maxrss / (1 << 30)
+    times = resource_usage.ru_utime + resource_usage.ru_stime
+    print(f'Peak Memory Usage = {mem:.3f} GB')
+    print(f'User Mode Time + System Mode Time = {times:.2f}s')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        main(sys.argv[1])
